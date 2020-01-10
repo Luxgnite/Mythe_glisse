b1340ced85d4fff7e121b8b53e583ad08506f572
Luxgnite <ewan.dubois@live.fr>
2020-01-10 13:08:19 +0100

    Character Controller
    * Can move the player character left & right, sensible to analogue input
    * Affected by gravity

---
+++ Assets/Scripts/Player/MovementController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    Transform character;
+    public float speed = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        character = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        character.Translate(new Vector3(speed * Input.GetAxis("Horizontal") *Time.deltaTime ,0f,0f));
+    }
+}
