aa26e66c12a476792fb58267d515bc65bc8a40af
Luxgnite <ewan.dubois@live.fr>
2020-01-10 13:15:07 +0100

    Camera Controller
    * Follow an object movement on X & Y axis
    * By default, follow the Player Character

---
+++ Assets/Sprites/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject targetFollow;
+    Transform targetFollowTransform;
+
+    void Start()
+    {
+        targetFollowTransform = targetFollow.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(targetFollowTransform.position.x, targetFollowTransform.position.y,-10);
+    }
+}
