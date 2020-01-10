95855fd03a8a3dba3c20901cd5c1c1e3c4fe7eab
Luxgnite <ewan.dubois@live.fr>
2020-01-10 15:36:55 +0100

    Pseudo Game Management
    * Setting of a pseudo Game Manager
    * GameManager can Reload a level, and can show a Level End Screen
    * Created a Level End Point
    * The testing level has an end

---
+++ Assets/Scripts/GameFlow/EndLevel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndLevel : MonoBehaviour
+{
+    public GameManager gameManager;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("collision");
+        if (collision.gameObject.tag == "Player")
+            gameManager.EndLevel();
+    }
+
+}
---
+++ Assets/Scripts/GameFlow/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject camera;
+    public GameObject[] guiElements;
+    public GameObject player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void EndLevel()
+    {
+        player.GetComponent<MovementController>().enabled = false;
+        player.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        guiElements[0].SetActive(true);
+        guiElements[1].SetActive(true);
+        camera.GetComponent<AudioSource>().Play();
+    }
+}
