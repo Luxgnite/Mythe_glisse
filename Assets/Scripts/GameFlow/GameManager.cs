6a796a4f8046b36bbe9aa91fe65cabf58707e8df
Luxgnite <ewan.dubois@live.fr>
2020-01-15 17:10:28 +0100

    Small tweaks
    * Fixed the missing GameObject ref on GameManager after reloading the level (temp fix)
    * Change a bit the level layout

--- Assets/Scripts/GameFlow/GameManager.cs
+++ Assets/Scripts/GameFlow/GameManager.cs
@@ -2,46 +2,47 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance = null;
 
     public GameObject camera;
-    public GameObject[] guiElements;
     public GameObject player;
+
+    public Text gameOverText;
+    public Button restartButton;
 
     void Awake()
     {
         if (instance == null)
-
             instance = this;
-
         else if (instance != this)
             Destroy(gameObject);
 
-        DontDestroyOnLoad(gameObject);
+        //DontDestroyOnLoad(gameObject);
 
         InitGame();
     }
 
     public void InitGame()
     {
+        Debug.Log("Initializing game...");
         player = GameObject.FindGameObjectWithTag("Player");
     }
 
     public void RestartLevel()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-        InitGame();
     }
 
     public void EndLevel()
     {
         player.GetComponent<MovementController>().enabled = false;
         player.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
-        guiElements[0].SetActive(true);
-        guiElements[1].SetActive(true);
+        gameOverText.gameObject.SetActive(true);
+        restartButton.gameObject.SetActive(true);
         camera.GetComponent<AudioSource>().Play();
     }
 }
