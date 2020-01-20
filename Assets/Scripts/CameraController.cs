77bffaaf417e530433e2674c307c45db8d6be739
Luxgnite <ewan.dubois@live.fr>
2020-01-20 02:04:08 +0100

    Background Parallax
    * Background now move with parallax!
    * Can have multiple layers
    * Now only have 1 layer of background

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -15,7 +15,7 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        transform.position = new Vector3(targetFollowTransform.position.x, targetFollowTransform.position.y,-10);
+        transform.position = new Vector3(targetFollowTransform.position.x, targetFollowTransform.position.y + 2.5f,-10);
 
     }
 }
